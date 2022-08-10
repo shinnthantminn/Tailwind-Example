6e40fa05236b5b56c6bd7a726debbca584982396
shinnthantminn <85192394+shinnthantminn@users.noreply.github.com>
2022-08-10 15:09:16 +0630

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#cb3b3b",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
